23df9db19d4d8da7f31566b945386f080d47a3e6
Jose Lopez <jlopez@three29.com>
2017-11-15 16:40:22 -0800

    Finished albums controller

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -9,9 +9,10 @@ class AlbumsController extends Controller {
     /**
      * Display a listing of the resource.
      *
-     *
+     * @return \Illuminate\Http\Response
      */
-    public function index() {
+    public function index()
+    {
 
         $albums = Albums::all();
 
@@ -19,6 +20,91 @@ class AlbumsController extends Controller {
 
     }
 
+    /**
+     * Get the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     *
+     */
+    public function show( $id )
+    {
+
+        $album = Albums::where( 'id', $id )->get();
+
+        if ( !empty( $album ) )
+        {
+
+            return response()->json( $album );
+
+        }
+        else {
+
+            return response()->json( [ 'status' => 'fail' ] );
+
+        }
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store( Request $request )
+    {
+
+        $this->validate( $request, [
+
+            'name' => 'required',
+
+            'description' => 'required',
+
+            'style' => 'required',
+
+            'published_on' => 'required'
+        ] );
+
+        $album = new Albums();
+
+        $album->name = $request->name;
+
+        $album->description = $request->description;
+
+        $album->category = $request->category;
+
+        $date = new \DateTime($request->published_on);
+
+        $dd = $date->format('Y-m-d');
+
+        $album->published_on = $dd;
+
+        $album->save();
+
+        return response()->json(['status' => 'success']);
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy( $id )
+    {
+
+        if( Albums::destroy( $id ) )
+        {
+
+            return response()->json( ['status' => 'success' ] );
+
+        }
+
+    }
 
 }
 
